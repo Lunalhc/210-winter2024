70dc59ae2812398f0d21ab62393f01241e2d1284
= <lunashih@byui.edu>
2024-01-10 12:22:10 -0700

    done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 100);
+        //Console.WriteLine(magic_number);
+
+        Console.WriteLine("What's your guess?");
+        string input = Console.ReadLine();
+        float guess = float.Parse(input); // store the guess number
+
+        while( guess != magic_number )
+        {
+
+           if(guess > magic_number){
+
+            Console.WriteLine("lower");
+            input = Console.ReadLine();
+            guess = float.Parse(input);
+
+           }
+
+           else if (guess < magic_number){
+
+              Console.WriteLine("higher");
+              input = Console.ReadLine();
+              guess = float.Parse(input);
+           }
+
+           else{
+              
+              input = Console.ReadLine();
+              guess = float.Parse(input);
+           }
+        }
+        
+        Console.WriteLine("You guessed it!");
     }
 }
