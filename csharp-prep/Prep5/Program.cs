7ada18e7028be77c354f6e8585228f8c0c635b2d
= <lunashih@byui.edu>
2024-01-10 12:29:41 -0700

    done

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome () {
+
+           Console.WriteLine("Welcome to the Program!");
+
+        }
+
+        static string PromptUserName() {
+
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            return name;
+            
+        }
+
+        static int PromptUserNumber() {
+
+            Console.WriteLine("What's your favorite number?");
+            int number = int.Parse(Console.ReadLine());
+            
+            return number;
+
+        }
+
+        static int SquareNumber(int integer){
+
+            int square = integer * integer;
+            return square;
+
+        }
+
+        DisplayWelcome();
+       
+
+        static void DisplayResult(){
+
+            string username =PromptUserName();
+
+            int inputnumber =  PromptUserNumber();
+            int resultnumber = SquareNumber(inputnumber); 
+            Console.WriteLine($"{username} ,the square of your number is {resultnumber}"); 
+
+
+        }
+
+        
+       
+    
+        DisplayResult();
     }
 }
