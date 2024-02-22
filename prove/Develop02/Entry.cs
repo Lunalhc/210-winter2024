4d81db3c2b38c023011123ac438891df92d73379
= <lunashih@byui.edu>
2024-02-22 12:16:13 -0700

    let's see if it will work this time

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,64 +1,8 @@
-using System.IO; 
-
-public class Entry{
 
 
-public string _date;
-public string _name;
-public string _prompt;
-public string _response;
-
-
-public void SetDate()
+public class Entry
 {
-    _date = DateTime.Now.ToShortDateString();
-}
-
-public void SetName(string name)
-{
-    _name = name;
-}
-
-
-public void SetPrompt(string prompt)
-{
-    _prompt = prompt;
-}
-
-
-public void SetResponse(string response)
-{
-    _response = response;
-}
-
-
-public string GetDate()
-{
-    return _date;
-}
-
-
-public string GetName()
-{
-    return _name;
-}
-
-
-public string GetPrompt()
-{
-    return _prompt;
-}
-
-
-public string GetResponse()
-{
-    return _response;
-}
-
-
-
-
-      
-
-
+    public string _Date;
+    public string _Name;
+    public string _Response;
 }
